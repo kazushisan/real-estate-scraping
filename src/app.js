096d3123ca600467e0b9077a70bb1c588c3b6174
Kazushi Konosu <kazushi@konosu.me>
2020-11-18 02:15:00 +0900

    show 100 per page

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ import getProperties from './getProperties.js'
 const sleep = (time) => new Promise((resolve) => setTimeout(resolve, time))
 
 let link =
-  'https://suumo.jp/jj/chintai/ichiran/FR301FC005/?shkr1=03&shkr3=03&cb=0.0&shkr2=03&smk=&mt=9999999&sc=13104&ar=030&bs=040&shkr4=03&ct=9999999&md=01&md=02&cn=9999999&ta=13&mb=0&fw2=&et=9999999'
+  'https://suumo.jp/jj/chintai/ichiran/FR301FC005/?ar=030&bs=040&ta=13&sc=13104&cb=0.0&ct=9999999&mb=0&mt=9999999&md=01&md=02&et=9999999&cn=9999999&shkr1=03&shkr2=03&shkr3=03&shkr4=03&sngz=&po1=25&po2=99&pc=100'
 
 const app = async () => {
   const browser = await puppeteer.launch()
